fdcfc49f0b6b43184f5790315a472f6d5beade8b
montree <scoopy_825@hotmail.com>
2016-02-20 14:02:20 +0700

    Explicit เสร็จแล้ววว

--- app/src/main/java/treconweb/puttan/montree/mytraffic/MainActivity.java
+++ app/src/main/java/treconweb/puttan/montree/mytraffic/MainActivity.java
@@ -2,12 +2,20 @@ package treconweb.puttan.montree.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   // Main Method
+
+}   // Main Class
