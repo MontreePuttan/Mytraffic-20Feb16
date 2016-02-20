f179248ae0eae70c59bac94ccf851efdd4550a70
montree <scoopy_825@hotmail.com>
2016-02-20 15:07:24 +0700

    Finish 20Feb16

--- app/src/main/java/treconweb/puttan/montree/mytraffic/MainActivity.java
+++ app/src/main/java/treconweb/puttan/montree/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package treconweb.puttan.montree.mytraffic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -21,8 +25,34 @@ public class MainActivity extends AppCompatActivity {
         bindWidget();//Create method [option+enter]  //Compleatement [shift+cmd+enter]
 
 
+        //Button Controller
+        buttonController();
+
+
+
 
     }   // Main Method
+
+    private void buttonController() {
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            //กด ctrl+space หลัง new
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                mediaPlayer.start();
+
+                //Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/VTj_glwit6M"));
+                startActivity(intent);
+
+
+
+            }   //event
+        });
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
