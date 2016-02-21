65bd39f35f7e518dc5989cf79607187cc93b1f32
montree <scoopy_825@hotmail.com>
2016-02-21 14:50:10 +0700

    Show View Finish

--- app/src/main/java/treconweb/puttan/montree/mytraffic/DetailActivity.java
+++ app/src/main/java/treconweb/puttan/montree/mytraffic/DetailActivity.java
@@ -2,12 +2,68 @@ package treconweb.puttan.montree.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
+    private Button preButton, backButton, nextButton;
+    private int[] iconInts;
+    private String[] titleStrings, detailLongStrings;
+    private int myIndexAnInt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-}
+
+        //Bind Widget
+        bindWidget();
+
+
+        //Show View
+        showView();
+
+    }   // Main Method
+
+    private void showView() {
+
+        //Receive Value From Intent
+        titleStrings = getIntent().getStringArrayExtra("Title");
+        iconInts = getIntent().getIntArrayExtra("Image");
+        myIndexAnInt = getIntent().getIntExtra("Index",0);
+
+        detailLongStrings = getResources().getStringArray(R.array.detail_long);
+
+        changeView(myIndexAnInt);
+
+
+
+    }   // showView
+
+    private void changeView(int  myIndexAnInt) {
+
+        titleTextView.setText(titleStrings[myIndexAnInt]);
+        trafficImageView.setImageResource(iconInts[myIndexAnInt]);
+        detailTextView.setText(detailLongStrings[myIndexAnInt]);
+
+
+    }   // changeView
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+        preButton = (Button) findViewById(R.id.button2);
+        backButton = (Button) findViewById(R.id.button3);
+        nextButton = (Button) findViewById(R.id.button4);
+
+    }   // bindWidget
+
+}    // Main Class
